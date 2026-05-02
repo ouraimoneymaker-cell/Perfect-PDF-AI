9fe4bf4441fa14359fd0ceb2b4b45aef7cf08f89
Sheis217 <ouraimoneymaker@gmail.com>
2026-05-02 19:24:18 -0400

    Use expanded Dr Dan template during PDF fill

--- app/pdf_fill.py
+++ app/pdf_fill.py
@@ -1,6 +1,7 @@
 import fitz  # PyMuPDF
 
-from app.prompt_parser import parse_label_answer_prompt, get_answer
+from app.prompt_parser import parse_label_answer_prompt
+from app.templates import build_dr_dan_template
 
 
 def fit_text_to_box(page, text, rect):
@@ -25,24 +26,16 @@ def fill_pdf_with_template(input_pdf, output_pdf, prompt_text):
     doc = fitz.open(input_pdf)
 
     answers = parse_label_answer_prompt(prompt_text)
-
-    # BASIC TEMPLATE (expand next)
-    template = [
-        {
-            "page": 0,
-            "rect": [100, 100, 300, 120],
-            "value": get_answer(answers, "name")
-        },
-        {
-            "page": 0,
-            "rect": [100, 140, 300, 160],
-            "value": get_answer(answers, "date of birth", "dob")
-        },
-    ]
+    template = build_dr_dan_template(answers)
 
     for field in template:
-        page = doc[field["page"]]
-        rect = fitz.Rect(field["rect"])
-        fit_text_to_box(page, field["value"], rect)
+        page_index = field["page"]
+        if page_index < 0 or page_index >= len(doc):
+            continue
 
-    doc.save(output_pdf)
+        page = doc[page_index]
+        rect = fitz.Rect(field["rect"])
+        value = field.get("value", "")
+        fit_text_to_box(page, value, rect)
+
+    doc.save(output_pdf, garbage=4, deflate=True)
